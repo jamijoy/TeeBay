9a1ec8bf73bba0656f51417b1fb5712c90953e87
jamijoy <jamijoyy@gmail.com>
2023-07-14 15:26:28 +0600

    Product Buy, Add, Borrow functionality added

--- TeeBay_api-gateway/app/Http/Controllers/Product/ProductController.php
+++ TeeBay_api-gateway/app/Http/Controllers/Product/ProductController.php
@@ -56,9 +56,53 @@ class ProductController extends Controller
      */
     public function edit(Request $request)
     {
-        $effected_count = DB::table('products')
-            ->where('id', $request->id)
-            ->decrement('quantity');
+        $effected_count = 0;
+
+        if ($request->action == 'buy'){
+
+            /*
+             * Function BUY
+             * user uses this feature to consume product
+             * so the quantity decreases
+             * */
+
+            $effected_count = DB::table('products')
+                ->where('id', $request->id)
+                ->decrement('quantity');
+        }else if ($request->action == 'add'){
+
+            /*
+             * Function ADD
+             * user uses this feature to add product to inventory
+             * so the quantity increases
+             * */
+
+            $effected_count = DB::table('products')
+                ->where('id', $request->id)
+                ->increment('quantity');
+        }else if ($request->action == 'borrow'){
+
+            /*
+             * Function BORROW
+             * user uses this feature to borrow product from inventory it will be given back later
+             * so the quantity decreases and borrow amount increases to use later
+             * */
+
+            $quantity = DB::table('products')
+                ->select('quantity')
+                ->where('id', $request->id)
+                ->value('quantity');
+
+            if ($quantity>0){
+                $effected_count_one = DB::table('products')
+                    ->where('id', $request->id)
+                    ->decrement('quantity');
+                $effected_count = DB::table('products')
+                    ->where('id', $request->id)
+                    ->increment('borrowed');
+            }
+
+        }
 
         if ($effected_count==1){
             $message = 'success';
